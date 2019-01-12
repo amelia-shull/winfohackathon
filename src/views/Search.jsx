3ab3b0b89f3199fd45c69832950f2e6dedc87460
Kateka Seth <katekaseth@gmail.com>
2019-01-12 15:19:34 -0800

    search bar

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,28 +1,80 @@
 import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
 import Header from "components/Header/Header.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
-
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+import ProductSection from "./LandingPage/Sections/ProductSection.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import './bootstrap.css';
+import "./Search.css";
 const dashboardRoutes = [];
 
 export class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {search: ''};
+    }
+
+    handleChange = (event) => {
+        console.log(this.state.search);
+        this.setState({search: event.target.value});
+    }
+
     render() {
         const { classes, ...rest } = this.props;
         return (
             <div>
-            <Header
-                color="transparent"
-                routes={dashboardRoutes}
-                brand="Name goes here"
-                rightLinks={<HeaderLinks />}
-                fixed
-                changeColorOnScroll={{
-                    height: 400,
-                    color: "white"
-                }}
-                {...rest}
+                <Header
+                    color="white"
+                    routes={dashboardRoutes}
+                    brand="Name goes here"
+                    rightLinks={<HeaderLinks />}
+                    fixed
+                    {...rest}
                 />
+            <div class="jumbotron"></div>
+                <div class="container">
+                    <form>
+                        <div class="form-group">
+                            <label for="search">Search makeup brands</label>
+                            <input type="text" className="form-control" 
+                                    value={this.state.search}
+                                    onChange={this.handleChange}
+                                    placeholder="Enter brand"></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary btn-md">Search</button>
+                    </form>
+            </div>
+            <ProductList search={this.state.search}></ProductList>
+            <Footer></Footer>
             </div>
         )
+    }
+}
+
+class ProductList extends React.Component {
+    render() {
+        return (
+        <div>
+            
+        </div>);
+    }
+}
+
+class ProductItem extends React.Component {
+    render() {
+        return(
+        <div>   
+
+        </div>);
     }
 }
 
