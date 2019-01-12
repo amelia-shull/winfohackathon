8beac7ad5a714ac7b52e84a2a9a29c7fa993ce00
amelia-shull <ashull@uw.edu>
2019-01-12 15:19:37 -0800

    game

--- src/views/Quiz.jsx
+++ src/views/Quiz.jsx
@@ -11,8 +11,9 @@ import CardBody from "components/Card/CardBody.jsx";
 import imagesStyles from "assets/jss/material-kit-react/imagesStyles.jsx";
 
 import { cardTitle } from "assets/jss/material-kit-react.jsx";
-
 import { brands } from "../brands.js";
+import "./quiz-style.css";
+
 
 const style = {
   ...imagesStyles,
@@ -24,39 +25,77 @@ const dashboardRoutes = [];
 export class Quiz extends React.Component {
     constructor() {
         super();
-        this.state = {round: 0, answered: false, correct: false};
+        this.state = {round: 0, answered: false, correct: false, number: 0};
     }
 
     noClick() { // they test on animals
         let round = this.state.round;
         if (!brands[round].crueltyFree) {
-            this.setState({round: this.state.round, answered: true, correct: true});
+            this.setState({round: this.state.round, answered: true, correct: true, number: this.state.number + 1});
         } else {
-            this.setState({round: this.state.round, answered: true, correct: false});
+            this.setState({round: this.state.round, answered: true, correct: false, number: this.state.number});
         }
     }
 
     yesClick() { // they are cruelty-free
         let round = this.state.round;
         if (brands[round].crueltyFree) {
-            this.setState({round: this.state.round, answered: true, correct: true});
+            this.setState({round: this.state.round, answered: true, correct: true, number: this.state.number + 1});
         } else {
-            this.setState({round: this.state.round, answered: true, correct: false});
+            this.setState({round: this.state.round, answered: true, correct: false, number: this.state.number});
         }
     }
     
     next() {
-        this.setState({round: this.state.round + 1, answered: false, correct: false});
+        this.setState({round: this.state.round + 1, answered: false, correct: false, number: this.state.number});
+    }
+
+    resetGame() {
+        this.setState({round: 0, answered: false, correct: false, number: 0})
     }
 
     render() {
+
+        if (this.state.round  == brands.length) {
+            return(
+                <div className="quiz">
+                <Header className="quiz-header"
+                    color="white"
+                    routes={dashboardRoutes}
+                    rightLinks={<HeaderLinks />}
+                    brand="Brand goes here"
+                    fixed
+                    changeColorOnScroll={{
+                        height: 400,
+                        color: "white"
+                    }}
+                    {...rest}
+                />
+                <div className="transparent-box">
+                    <h2>Game Over!</h2>
+                    <Card style={{width: "20rem"}} className="card">
+                        <CardBody>
+                            <h3>{this.state.number + " correct out of " + brands.length}</h3>
+                            <Button color="primary" onClick={() => this.resetGame()}>Play Again</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+            )
+        }
+
+
+
+
         const { classes, ...rest } = this.props;
         let url = brands[this.state.round].logo;
         url = require('./makeup-logos/' + url);
 
         let backroundColor = !this.state.answered ? "white" : this.state.correct ? "green" : "red";
         let buttons = <Button color="primary" onClick={() => this.next()}>Next</Button>;
-        let text = !this.state.answered ? "Is this brand cruelty free?" : this.state.correct ? "Correct" : "Incorrect";
+        let isCrueltyFree = brands[this.state.round].crueltyFree ? " is cruelty-free!": " is not cruelty-free :("
+        isCrueltyFree = brands[this.state.round].brand + isCrueltyFree;
+        let text = !this.state.answered ? "Is this brand cruelty free?" : this.state.correct ? "Correct. " + isCrueltyFree: "Incorrect. " + isCrueltyFree;
         if (!this.state.answered) {
             buttons = <div>
                 <Button color="success" onClick={() => this.yesClick()}>Yes</Button>
@@ -65,11 +104,12 @@ export class Quiz extends React.Component {
         }
 
         return (
-            <div>
-                <Header
-                    color="transparent"
+            <div className="quiz">
+                <Header className="quiz-header"
+                    color="white"
                     routes={dashboardRoutes}
                     rightLinks={<HeaderLinks />}
+                    brand="Brand goes here"
                     fixed
                     changeColorOnScroll={{
                         height: 400,
@@ -77,17 +117,20 @@ export class Quiz extends React.Component {
                     }}
                     {...rest}
                 />
-                <Card style={{width: "20rem"}}>
-                    <img
-                        style={{height: "180px", width: "auto", display: "block"}}
-                        src={url}
-                        alt="Makeup brand logo"
-                    />
-                    <CardBody>
-                        <h4>{text}</h4>
-                        {buttons}
-                    </CardBody>
-                </Card>
+                <div className="transparent-box">
+                    <h2>Test your knowledge on cruelty-free brands!</h2>
+                    <Card style={{width: "20rem"}} className="card">
+                        <img
+                            style={{height: "180px", width: "auto", display: "block"}}
+                            src={url}
+                            alt="Makeup brand logo"
+                        />
+                        <CardBody>
+                            <h4>{text}</h4>
+                            {buttons}
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         )
     }
