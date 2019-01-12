0ca7c66480c61f00b8065f13873ec088ca3d96c7
Kateka Seth <katekaseth@gmail.com>
2019-01-12 12:58:19 -0800

    make other pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,12 @@ import Components from "views/Components/Components.jsx";
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
+import { Search } from "views/Search.jsx";
+import { Quiz } from "views/Quiz.jsx";
 
 var indexRoutes = [
-  { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
-  { path: "/login-page", name: "LoginPage", component: LoginPage },
+  { path: "/search", name: "Search", component: Search },
+  { path: "/quiz", name: "Quiz", component: Quiz },
   { path: "/", name: "LandingPage", component: LandingPage }
 ];
 
---
+++ src/views/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Search extends React.Component {
+    render() {
+        return <h1>hello please work</h1>
+
+    }
+}
+
+export default Search;
