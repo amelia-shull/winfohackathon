26446b43387d0b66b5df6b393d339ed08ff42096
Kateka Seth <katekaseth@gmail.com>
2019-01-12 12:13:09 -0800

    change routing

--- src/index.js
+++ src/index.js
@@ -7,22 +7,16 @@ import LandingPage from "views/LandingPage/LandingPage.jsx";
 import indexRoutes from "routes/index.jsx";
 
 import "assets/scss/material-kit-react.css?v=1.3.0";
-import LandingPage from "./views/LandingPage/LandingPage";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  
-  /*<Router history={hist}>
+  <Router history={hist}>
     <Switch>
       {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={Home} />;
+        return <Route path={prop.path} key={key} component={LandingPage} />;
       })}
     </Switch>
   </Router>,
-  */
-  <LandingPage></LandingPage>,
   document.getElementById("root")
-  
-
 );
